b4a5f388149869f0d4cd9a5952c1950983a4cf85
EzhilParivallal <ezhilparivallal17@gmail.com>
2025-01-14 06:27:44 +0530

    First Commit

--- src/test/java/pages/CartPage.java
+++ src/test/java/pages/CartPage.java
@@ -43,17 +43,28 @@ public class CartPage extends ProjectSpecificationMethods{
     }
     public CartPage deleteProduct(String ProductNm) throws InterruptedException {
     	WebElement table = driver.findElement(By.xpath("//table[@class='table table-bordered table-hover table-striped']"));
-		List<WebElement> rows = table.findElements(By.xpath("//tr"));
-		List<WebElement> cols = table.findElements(By.xpath("//th"));
-		int row_size = rows.size();
-		String Product = null;
-		String CartTotal=driver.findElement(By.xpath("//h3[@id='totalp']")).getText();
+		List<WebElement> rows; 
+		//List<WebElement> cols = table.findElements(By.xpath("//th"));
+		int row_size;
+		String Product = null,CartTotal,CartTotalFinal;
+		WebElement CartVal=driver.findElement(By.xpath("//h3[@id='totalp']"));
+		if(CartVal.isDisplayed()) {
+		    CartTotal=CartVal.getText();
+		}
+		else {
+			CartTotal="";
+		}
 		if(CartTotal.isEmpty()) {
 			CartValue=0;
 		}else {
 		CartValue=Integer.parseInt(CartTotal);
 		expCartValue=CartValue;}
-		for(int i=1;i<row_size;i++) {
+		rows= table.findElements(By.xpath("//tr"));
+		row_size= rows.size();
+			
+			for(int i=1;i<row_size;i++) {
+				Thread.sleep(5000);
+				if(table.findElement(By.xpath("//tr["+i+"]//td[2]")).isDisplayed()) {
 			cell = table.findElement(By.xpath("//tr["+i+"]//td[2]"));
 			String cell_val = cell.getText();
 			if(cell_val.equals(ProductNm)) {
@@ -62,14 +73,26 @@ public class CartPage extends ProjectSpecificationMethods{
 				ProductPrice=table.findElement(By.xpath("//tr["+i+"]//td[3]")).getText();
 				table.findElement(By.xpath("//tr["+i+"]//td[4]//a[text()='Delete']")).click();
 				Thread.sleep(5000);
+				rows= table.findElements(By.xpath("//tr"));
+				row_size= rows.size();
 				expCartValue=expCartValue-Integer.parseInt(ProductPrice);
 				}
-	   }
-		String CartTotalFinal=driver.findElement(By.xpath("//h3[@id='totalp']")).getText();
+			if(row_size<2) {
+				break;
+			}
+				}
+			}
+			if(CartVal.isDisplayed()) {
+		      CartTotalFinal=CartVal.getText();
+			}
+			else {
+				CartTotalFinal="";
+			}
 		if(CartTotalFinal.isEmpty()) {
 			CartValueFinal=0;
 		}else {
 		CartValueFinal=Integer.parseInt(CartTotalFinal);}
+		System.out.println(CartValueFinal+" "+expCartValue);
 		return this;
     }
     public CartPage validateDelete(String ProductNm) {
